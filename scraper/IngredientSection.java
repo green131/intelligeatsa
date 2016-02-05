0b8a293e8dd0469b492c76599d7d28b857aa440f
Pranav Menon <menon1@purdue.edu>
2016-02-05 15:39:03 -0500

    add/fix scraper files

---
+++ scraper/IngredientSection.java
@@ -0,0 +1,20 @@
+package scraper;
+
+import java.util.ArrayList;
+
+//i.e: Making the sauce, or making the bread
+public class IngredientSection {
+	String sectionName;
+	ArrayList<Ingredient> sectionIngredients;
+	
+	public IngredientSection(String sectionName, ArrayList<Ingredient> sectionIngredients){
+		this.sectionName = sectionName;
+		this.sectionIngredients = sectionIngredients;
+	}
+	
+	@Override
+	public String toString(){
+		return "(sectionName = " + sectionName + ", sectionIngredients: " + sectionIngredients + ")";
+	}
+	
+}
