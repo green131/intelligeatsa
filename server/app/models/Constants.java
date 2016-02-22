a097a85aa3053303ec24a3cac649dd1cf9ef4b13
green131 <green131@purdue.edu>
2016-02-21 20:14:03 -0500

    Added get recipe by id and get requesting by url

--- server/app/models/Constants.java
+++ server/app/models/Constants.java
@@ -15,4 +15,9 @@ public class Constants {
     public static final String USERS_COLLECTION = "users";
   }
 
+  public static final class Recipe {
+    public static final String KEY_TITLE = "title";
+    public static final String INDEX_TITLE_TEXT = "title_text";
+  }
+
 }
