946870b316c4325879384794ae742caa146278a3
Mayank Jethva <jethva@purdue.edu>
2016-02-23 19:46:06 -0500

    app/pages/searchResultsPage/

--- web/app/pages/searchResultsPage/searchResultsPage.js
+++ web/app/pages/searchResultsPage/searchResultsPage.js
@@ -6,7 +6,7 @@ var pagesModule = angular.module('intelligeatsa.pages');
 pagesModule.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/search/:query',{
     templateUrl: 'pages/searchResultsPage/searchResultsPage.html',
-     controller: new SearchResultsPageController(),
+     controller: SearchResultsPageController,
      controllerAs: '$ctrl'
   });
 }]);
@@ -18,36 +18,37 @@ function SearchResultsPageController($http,$routeParams){
   ctrl.query = $routeParams.query;
   ctrl.rangeStart = 0;
   ctrl.rangeEnd = 20;
+
+  ctrl.next = function(){
+    if(ctrl.recipes.length == 20){
+    ctrl.rangeStart+=20;
+    ctrl.rangeEnd+=20;
+    ctrl.getRecipes();
+    }
+  };
+  ctrl.previous = function(){
+    if(ctrl.rangeStart > 0 && ctrl.rangeEnd > 20){
+    ctrl.rangeStart-=20;
+    ctrl.rangeEnd-=20;
+    ctrl.getRecipes();
+    }
+  };
+
+  ctrl.getRecipes = function(){
+    var recipeSearchUrl = 'http://localhost:8080/recipe/search/'+ ctrl.query + '/'+ctrl.rangeStart+'/'+ctrl.rangeEnd;
+    $http({
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    url: recipeSearchUrl,
+  }).then(function successCallback(response) {
+      ctrl.recipes = response.data;
+      if(ctrl.recipes.length < 20){
+        ctrl.lastPage = true;
+      }
+    }, function errorCallback(response) {
+      console.log(response);
+    });
+  };
+
   ctrl.getRecipes();
 }
-
-SearchResultsPageController.prototype.next = function(){
-  if(ctrl.recipes.length == 20){
-  ctrl.rangeStart+=20;
-  ctrl.rangeEnd+=20;
-  ctrl.getRecipes();
-  }
-};
-SearchResultsPageController.prototype.previous = function(){
-  if(ctrl.rangeStart > 0 && ctrl.rangeEnd > 20){
-  ctrl.rangeStart-=20;
-  ctrl.rangeEnd-=20;
-  ctrl.getRecipes();
-  }
-};
-
-SearchResultsPageController.prototype.getRecipes = function(){
-  var recipeSearchUrl = 'http://localhost:8080/recipe/search/'+ ctrl.query + '/'+ctrl.rangeStart+'/'+ctrl.rangeEnd;
-  $http({
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  url: recipeSearchUrl,
-}).then(function successCallback(response) {
-    ctrl.recipes = response.data;
-    if(ctrl.recipes.length < 20){
-      ctrl.lastPage = true;
-    }
-  }, function errorCallback(response) {
-    console.log(response);
-  });
-};
