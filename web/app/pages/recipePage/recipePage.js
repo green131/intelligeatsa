f1659ce89eee49d57f79a61cf2cb1451d2dc3bc8
Mayank Jethva <jethva@purdue.edu>
2016-03-06 22:39:22 -0500

    working on rating

--- web/app/pages/recipePage/recipePage.js
+++ web/app/pages/recipePage/recipePage.js
@@ -1,9 +1,8 @@
 'use strict';
 
-var pagesModule = angular.module('intelligeatsa.pages');
-
-// attach to url route
-pagesModule.config(['$routeProvider', function($routeProvider) {
+angular.module('intelligeatsa.pages')
+.constant('apiRateUrl','http://localhost:8080/recipe/')
+.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/recipe/:id',{
     templateUrl: 'pages/recipePage/recipePage.html',
     controller: RecipePageController,
@@ -11,10 +10,11 @@ pagesModule.config(['$routeProvider', function($routeProvider) {
   });
 }]);
 
-function RecipePageController($http, $routeParams){
+function RecipePageController($http, $routeParams,apiRateUrl,userSession){
   var ctrl = this;
   var recipeUrl = "http://localhost:8080/recipe/id/" + $routeParams.id;
   ctrl.recipe = '';
+  ctrl.noRating = false;
   ctrl.instructionList=[];
   $http({
   method: 'POST',
@@ -22,8 +22,47 @@ function RecipePageController($http, $routeParams){
   }).then(function successCallback(response) {
       ctrl.recipe = response.data;
       ctrl.instructionList = ctrl.recipe.preparation[0];
-      console.log(response);
+      ctrl.recipeId = ctrl.recipe._id.$oid;
+      // round
+      if(ctrl.recipe.hasOwnProperty('rating')){
+        ctrl.recipe.rating.value = Math.floor(ctrl.recipe.rating.value);
+      }else{
+        ctrl.noRating = true;
+      }
   }, function errorCallback(response){
     console.log(response);
   });
+
+  ctrl.rateRecipe = function(rating){
+    if(!userSession.sessionExists()){
+      $('#loginModal').modal('show');
+      return;
+    }
+    var user = userSession.getUser();
+    console.log(user);
+    $http({
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      url: (apiRateUrl + ctrl.recipeId + '/rate/' + rating),
+      data:{
+        userID:user.token
+      }
+    }).then(function success(response){
+      if(ctrl.noRating){
+        ctrl.recipe.rating = {
+          value: 0,
+          numOfRaters:0
+        };
+        ctrl.noRating = false;
+      }
+      var originalSum = (ctrl.recipe.rating.value * ctrl.recipe.rating.numOfRaters);
+      var updatedSum = originalSum + rating;
+      ctrl.recipe.rating.numOfRaters++;
+      var newRating = Math.floor(updatedSum / ctrl.recipe.rating.numOfRaters);
+      ctrl.recipe.rating.value = newRating;
+      console.log(response);
+    }, function error(response){
+      console.log(response);
+    });
+  };
 }
